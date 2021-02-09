f26634864cccb7aa3001e86d3626d912d2d528cf
Michael Chen <michaelchen@gatech.edu>
2021-02-09 16:08:18 -0500

    TypeScript starter (#11)
    * Copy files from https://github.com/microsoft/TypeScript-Node-Starter
    * Delete the views folder
    * npm audit fix --force
    * npm upgrade and add secrets commands
    * Delete unnecessary files

---
+++ src/controllers/api.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+import { Response, Request, NextFunction } from "express";
+import { UserDocument } from "../models/User";
+
+
+/**
+ * List of API examples.
+ * @route GET /api
+ */
+export const getApi = (req: Request, res: Response) => {
+    res.render("api/index", {
+        title: "API Examples"
+    });
+};
