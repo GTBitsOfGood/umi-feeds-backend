40bcd8c8aabdf1a651c3e742ea30002d826ba4bf
Colin Tam <colintam@Colins-MacBook-Pro.local>
2021-02-13 15:51:29 -0800

    Added test endpoint and middleware for auth0

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,7 +1,11 @@
 'use strict';
 
 import { Response, Request, NextFunction } from 'express';
+import app from '../app';
 import { UserDocument } from '../models/User';
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+const bodyParser = require('body-parser');
 
 
 /**
@@ -13,3 +17,31 @@ export const getApi = (req: Request, res: Response) => {
         title: 'API Examples'
     });
 };
+
+// enable use of request body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// create middleware for checking the JWT
+const checkJwt = jwt({
+    // Dynamically provide a signing key
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://test-api/.well-known/jwks.json'
+    }),
+
+    // validate the audience and the issuer
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: 'https://test-api/',
+    algorithms: ['RS256']
+});
+
+app.post('/test-api', checkJwt, function(req, res){
+    res.status(201)
+    res.send("Authenticated")
+})
