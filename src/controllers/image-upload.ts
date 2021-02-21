18a53045befeae49d5e27dff2ed70db9e1eaeca1
judahkrug <jkrug7@gatech.edu>
2021-02-21 11:27:51 -0500

    Updated file type to UploadedFile for compatibility

--- src/controllers/image-upload.ts
+++ src/controllers/image-upload.ts
@@ -1,6 +1,7 @@
 import { Response, Request, NextFunction } from 'express';
 
 import { BlobServiceClient, ContainerClient} from '@azure/storage-blob';
+import { UploadedFile } from 'express-fileupload';
 
 const sasToken = process.env.storagesastoken || 'sv=2019-12-12&ss=b&srt=sco&sp=rwdlacx&se=2021-03-17T01:19:33Z&st=2021-02-16T18:19:33Z&spr=https&sig=1cPkIKyFMSghjuiMyHgiBFSC6pnKiW86U%2FlF6O%2Fgzcw%3D';
 const containerName = 'image-container';
@@ -25,19 +26,19 @@ const getBlobsInContainer = async (containerClient: ContainerClient) => {
   return returnedBlobUrls;
 };
 
-const createBlobInContainer = async (containerClient: ContainerClient, file: File) => {
+const createBlobInContainer = async (containerClient: ContainerClient, file: UploadedFile) => {
   
   // create blobClient for container
   const blobClient = containerClient.getBlockBlobClient(file.name);
 
   // set mimetype as determined from browser with file upload control
-  const options = { blobHTTPHeaders: { blobContentType: file.type } };
+  const options = { blobHTTPHeaders: { blobContentType: file.encoding } };
 
   // upload file
-  await blobClient.uploadBrowserData(file, options);
+  await blobClient.uploadBrowserData(file.data, options);
 };
 
-export const uploadFileToBlob = async (file: File | null): Promise<string[]> => {
+export const uploadFileToBlob = async (file: UploadedFile | null): Promise<string[]> => {
   if (!file) return [];
 
   // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
@@ -68,8 +69,9 @@ export const postImage = (req: Request, res: Response) => {
                 message: 'No file uploaded'
             });
         } else {
-            // const image = req.files.image;
-            // uploadFileToBlob(image);
+            const imgRequest = req.files.image as UploadedFile;
+            // const image: File = new File(imgRequest.data, imgRequest.name, {type: "text/plain", lastModified: date});
+            uploadFileToBlob(imgRequest);
         }
     } catch (err) {
         res.status(500).send(err);
