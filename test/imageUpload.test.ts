5c2d15a21879fd7f43c4c42e6393eafe02aef092
Michael Chen <michael.ld.chen@gmail.com>
2021-02-23 22:51:23 -0500

    Fix typo

--- test/imageUpload.test.ts
+++ test/imageUpload.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import app from '../src/app';
 
-describe('GET /upload', () => {
+describe('POST /upload', () => {
     it('should return 400 Bad Request', () => {
         return request(app).post('/upload')
             .expect(400);
