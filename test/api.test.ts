1a4f820efaac3cf88220d9850c701aee21da717a
Michael Chen <michael.ld.chen@gmail.com>
2021-02-23 22:18:58 -0500

    Delete obsolete tests

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest';
 import app from '../src/app';
 
-describe('GET /api', () => {
+describe('GET /api/donations', () => {
     it('should return 200 OK', () => {
-        return request(app).get('/api')
+        return request(app).get('/api/donations')
             .expect(200);
     });
 });
