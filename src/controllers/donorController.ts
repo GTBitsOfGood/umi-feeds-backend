461212c558c9bff2a4ad9754eabd3201735afab3
awang378 <andrewatga@gmail.com>
2021-02-26 17:11:25 -0500

    Functionality for uploading multiple images

--- src/controllers/donorController.ts
+++ src/controllers/donorController.ts
@@ -1,6 +1,11 @@
 import { Response, Request, NextFunction } from 'express';
 import { Donor } from '../models/Donor';
 import { Donation } from '../models/Donation';
+import { UploadedFile } from 'express-fileupload';
+import storage from 'azure-storage';
+import fs from 'fs';
+
+const containerName = 'image-container';
 
 /**
  * Gets Donations
@@ -61,7 +66,66 @@ export const getDonations = (req: Request, res: Response) => {
  * Posts Donations
  * @route POST /donations
  */
-export const postDonations = (req: Request, res: Response) => {
+export const postDonations = async (req: Request, res: Response) => {
+    try {
+        if (!req.files) {
+            res.status(400).json({
+                status: false,
+                message: 'No images uploaded'
+            });
+        } else if (!req.files.descriptionImage && !req.files.foodImage) {
+            res.status(400).send({
+                status: false,
+                message: 'No images attached to the key "descriptionImage" or "foodImage" in your request',
+                sentReq: String(req.files)
+            });
+        } else {
+            const blobSVC = storage.createBlobService(process.env.CONNECTION_STRING_AZURE);
+            const descriptionImages = req.files.descriptionImage as UploadedFile[];
+            const descriptionUrls: String[] = [];
+
+            for (let i = 0; i < descriptionImages.length; i++) {
+                const imgRequest = descriptionImages[i] as UploadedFile;
+
+                blobSVC.createBlockBlobFromText(containerName, imgRequest.name, imgRequest.data, (error: Error) => {
+                    if (error) {
+                        console.error(`Error in createBlockBlobFromText: ${error}`);
+                        return res.status(500).json({error: error.message});
+                    }
+                });
+                let url = "https://umifeedsimageupload.blob.core.windows.net/" + containerName + "/" + imgRequest.name; 
+                descriptionUrls.push(url)
+
+            }
+            /*
+            const foodImages = req.files.foodImage as UploadedFile[];
+            const foodUrls: String[] = [];
+
+            for (let i = 0; i < foodImages.length; i++) {
+                const imgRequest = foodImages[i] as UploadedFile;
+
+                blobSVC.createBlockBlobFromText(containerName, imgRequest.name, imgRequest.data, (error: Error) => {
+                    if (error) {
+                        console.error(`Error in createBlockBlobFromText: ${error}`);
+                        return res.status(500).json({error: error.message});
+                    }
+                });
+                let url = "https://umifeedsimageupload.blob.core.windows.net/" + containerName + "/" + imgRequest.name; 
+                foodUrls.push(url)
+
+            } 
+            res.status(200).json({success: true, result: descriptionUrls});
+            */
+        } 
+        const jsonData = req.files.test as UploadedFile;
+        let test = JSON.parse(jsonData.data.toString())
+        console.log(test)
+        res.status(200).json({success: true})
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(String(err));
+    }
+    /* 
     const donation = new Donation(req.body);
     return donation.save()
     .then(result => {
@@ -73,7 +137,8 @@ export const postDonations = (req: Request, res: Response) => {
         return res.status(500).json({
             message: error.message
         });
-    });
+    }); */
+
 };
 
 /**
