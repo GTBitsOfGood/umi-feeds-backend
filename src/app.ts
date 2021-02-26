e5443aaf0538c28315827a0d34ea57407f046738
Michael Ryan <michael_ryan_2000@yahoo.com>
2021-02-25 22:42:55 -0500

    Added ability to send push notifications from backend (#39)
    Made a new POST /testpush endpoint. After you edit the code to add your app's expo push token, it will notify the app with a dummy message.
    Co-authored-by: Michael Chen <michael.ld.chen@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import mongoose from 'mongoose';
 import passport from 'passport';
 import bluebird from 'bluebird';
 import { MONGODB_URI, SESSION_SECRET } from './util/secrets';
+import {sendBatchNotification} from './util/notifications';
 
 const MongoStore = mongo(session);
 
@@ -61,6 +62,11 @@ app.use(fileupload());
 
 // Routes
 app.post('/upload', imageController.postImage);
+app.post('/testpush', function(req, res) {
+    res.send('testing push notification');
+    // Add your ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx] to the array below to test sending push notifications to yourself. See the frontend console for a line like Expo Push Token : ExponentPushToken[sfdjiodojifsdojisdfjio]
+    sendBatchNotification('Umi Feeds (title)', 'this is a test (body)', ['']);
+});
 
 // Sub Routers
 import donorRouter from './routes/donors';
