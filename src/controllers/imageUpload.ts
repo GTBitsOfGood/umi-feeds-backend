e959882fe3a436b0085ec7950b95b33ca307cabb
Judah Krug <jkrug7@gatech.edu>
2021-04-06 17:13:04 -0400

    Create unit tests with in-memory Mongo, add unique ID to names of uploaded images (#81)
    Co-authored-by: Michael Chen <michael.ld.chen@gmail.com>

--- src/controllers/imageUpload.ts
+++ src/controllers/imageUpload.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from 'express';
 import { UploadedFile } from 'express-fileupload';
 import storage from 'azure-storage';
+import { uid } from 'uid';
 
 const containerName = 'image-container';
 
@@ -26,8 +27,30 @@ export const postImage = (req: Request, res: Response) => {
         } else {
             const imgRequest = req.files.image as UploadedFile;
             const blobSVC = storage.createBlobService(process.env.CONNECTION_STRING_AZURE);
+            // let unique = false;
+            const uniqueID: string = uid(11);
+            const index: number = imgRequest.name.lastIndexOf('.');
+            let blobName: string = imgRequest.name.substring(0, index);
+            blobName = blobName.concat('_', uniqueID, imgRequest.name.substring(index));
 
-            blobSVC.createBlockBlobFromText(containerName, imgRequest.name, imgRequest.data, (err: Error) => {
+            // TODO: This code is supposed to try to repeatedly a new UID if an image with this name and UID already exists. The while loop apparently does not terminate, unfortunately. I think we may want to use recursion. Anyway, the chances are pretty low that a file with this name already exists, and even if we overwrite an old file, it's not a huge deal.
+            // while (!unique) {
+            //     uniqueID = uid(11);
+            //     index = imgRequest.name.lastIndexOf('.');
+            //     blobName = imgRequest.name.substring(0, index) + '_' + uniqueID + imgRequest.name.substring(index);
+            //     unique = true;
+
+            //     blobSVC.doesBlobExist(containerName, blobName, function(error, result) {
+            //         if (!error) {
+            //           if (result.exists) {
+            //             unique = true;
+            //           } else {
+            //           }
+            //         }
+            //       });
+            // }
+
+            blobSVC.createBlockBlobFromText(containerName, blobName, imgRequest.data, (err: Error) => {
                 if (err) {
                     console.error(`Error in createBlockBlobFromText: ${err}`);
                     res.status(500).send(String(err));
